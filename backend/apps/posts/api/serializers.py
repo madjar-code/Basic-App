61a5042e7ac9a0e1af01c8c2241071f7baaecba8
Madjar Ivan <evanmadjar@gmail.com>
2023-05-03 12:11:01 +0300

    feat: Add serializer to post object

---
+++ backend/apps/posts/api/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from users.api.serializers import UserSerializer
+
+from posts.models import Post, Tag
+
+
+class TagSerializer(ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = (
+            'id',
+            'name',
+        )
+        read_only_fields = ('__all__')
+
+
+class PostSerializer(ModelSerializer):
+    author = UserSerializer(read_only=True)
+    tags = TagSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Post
+        fields = (
+            'title',
+            'slug',
+            'author',
+            'thumbnail',
+            'body',
+            'read_time',
+            'tags',
+            'created_at',
+        )
+        read_only_fields = ('author',)

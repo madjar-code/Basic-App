a15512ef34f1eae731bfc1192f445667b95901ce
Madjar Ivan <evanmadjar@gmail.com>
2023-05-04 18:20:02 +0300

    feat: Register Comment model in Django admin
    Comment model was registered in admin panel so that
    administrator can view the comments under the posts

--- backend/apps/comments/admin.py
+++ backend/apps/comments/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from comments.models import Comment
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'body',
+        'id',
+        'post',
+        'author',
+        'created_at',
+    )
+    list_filter = ('created_at',)
+    search_fields = (
+        'author',
+        'body',
+    )

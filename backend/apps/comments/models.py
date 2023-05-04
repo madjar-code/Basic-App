43f7b43543a22dd571a6cb5875ffbf37e7011de6
Madjar Ivan <evanmadjar@gmail.com>
2023-05-04 18:17:10 +0300

    feat: Add Comment models to comments app

--- backend/apps/comments/models.py
+++ backend/apps/comments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from common.mixins.models import UUIDModel
 
-# Create your models here.
+
+class Comment(UUIDModel):
+    post = models.ForeignKey(
+        'posts.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE, related_name='comments')
+    body = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return f'{self.id}. Comment by {self.author}'

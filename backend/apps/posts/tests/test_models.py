91913a0c28c158ed09df29180a62a511dd10abc3
Madjar Ivan <evanmadjar@gmail.com>
2023-05-13 16:52:30 +0300

    test: Test Post and Tag models in posts app
    The tests verified the creation of a post and
    the addition of tags to it

---
+++ backend/apps/posts/tests/test_models.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from users.models import User
+from posts.models import Post, Tag
+
+
+class TestModels(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test', email='test@test.com', password='test')
+
+        self.tag1 = Tag.objects.create(name='Tag 1')
+        self.tag2 = Tag.objects.create(name='Tag 2')
+
+    def test_tag_model(self):
+        """
+        New Tag does not change instance and is
+        saved to Database
+        """
+        tag = Tag.objects.create(name='Test tag')
+
+        self.assertIsInstance(tag, Tag)
+        self.assertEqual(Tag.objects.get(name='Test tag'), tag)
+
+    def test_post_model(self):
+        """
+        New Post does not change instance and is
+        saved to Database
+        """
+        post: Post = Post.objects.create(
+            title='Test Post', slug='test-post', thumbnail=\
+                'https://www.test.example.com', author=self.user,
+            body='Test content of the post', read_time=5, is_public=True)
+        post.tags.add(self.tag1)
+        post.tags.add(self.tag2)
+
+        self.assertIsInstance(post, Post)
+        self.assertEqual(Post.objects.get(slug='test-post').\
+            tags.get(name='Tag 1'), self.tag1)
+
+        self.assertEqual(Post.objects.get(slug='test-post'), post)

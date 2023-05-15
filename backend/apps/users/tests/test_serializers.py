ab426de7117f660c3b835b941dd9bcc94fac125a
Madjar Ivan <evanmadjar@gmail.com>
2023-05-15 15:08:25 +0300

    test: End of create tests for users app

--- backend/apps/users/tests/test_serializers.py
+++ backend/apps/users/tests/test_serializers.py
@@ -28,5 +28,157 @@ def generate_image_file() -> SimpleUploadedFile:
 class TestSerializers(TestCase):
     def setUp(self) -> None:
         self.user = User.objects.create_user(
-            username='test', 
+            username='test', email='test@test.com', password='test')
+        self.avatar = generate_image_file()
+        self.user_serializer = UserSerializer(data={
+            'username': 'testusername',
+            'first_name': 'Test',
+            'last_name': 'Testing',
+            'title': 'testtitle',
+            'avatar': self.avatar,
+            'is_staff': False,
+            'is_verified': True
+        })
+        self.user_password_serializer = UserPasswordSerializer(data={
+            'password': 'strongpassword',
+            'new_password': 'evenstrongpassword',
+        })
+        self.avatar_serializer = AvatarSerializer(
+            data={'avatar': self.avatar}
         )
+        self.create_user_serializer = CreateUserSerializer(data={
+            'username': 'testusername',
+            'email': 'testemail@example.com',
+            'password': 'test',
+            'first_name': 'Test',
+            'last_name': 'Tested',
+        })
+        self.user_details_serializer = UserDetailsSerializer(data={
+            'username': 'testusername',
+            'email': 'testemail@example.com',
+            'first_name': 'Test',
+            'last_name': 'Tested',
+        })
+
+    def test_user_serializer(self):
+        """
+        User serializer
+        """
+        serializer: UserSerializer = self.user_serializer
+        self.assertTrue(serializer.is_valid())
+
+        data = serializer.data
+
+        self.assertCountEqual(
+            data.keys(),
+            ['username',
+             'first_name',
+             'last_name',
+             'title',
+             'avatar',
+             'is_staff',
+             'is_verified',])
+        self.assertEqual(data['username'], 'testusername')
+        self.assertEqual(data['first_name'], 'Test')
+        self.assertEqual(data['last_name'], 'Testing')
+        self.assertEqual(data['title'], 'testtitle')
+        self.assertFalse(data['is_staff'])
+        self.assertTrue(data['is_verified'])
+
+    def test_user_password_serializer_new_password(self):
+        """
+        New password needs to be at least 6 characters long
+        """
+        serializer = self.user_password_serializer
+
+        serializer.initial_data['new_password'] = '1234'
+        self.assertFalse(serializer.is_valid())
+
+    def test_avatar_serializer(self):
+        """
+        Avatar Serializer
+        """
+        serializer = self.avatar_serializer
+
+        self.assertTrue(serializer.is_valid())
+
+        data = serializer.data
+
+        self.assertCountEqual(data.keys(), ['avatar'])
+
+    def test_create_user_serializer(self):
+        """
+        Create User serializer
+        """
+        serializer = self.create_user_serializer
+
+        self.assertTrue(serializer.is_valid())
+
+        data = serializer.data
+
+        self.assertCountEqual(
+            data.keys(), ['username', 'email', 'first_name', 'last_name'])
+        self.assertEqual(data['username'], 'testusername')
+        self.assertEqual(data['email'], 'testemail@example.com')
+        self.assertEqual(data['first_name'], 'Test')
+        self.assertEqual(data['last_name'], 'Tested')
+
+    def test_create_user_serializer_username(self):
+        """
+        Username has to be unique
+        """
+        serializer = self.create_user_serializer
+
+        serializer.initial_data['username'] = 'test'
+        self.assertFalse(serializer.is_valid())
+
+    def test_create_user_serializer_email(self):
+        """
+        E-Mail has to be unique
+        """
+        serializer = self.create_user_serializer
+
+        serializer.initial_data['email'] = 'test@test.com'
+        self.assertFalse(serializer.is_valid())
+
+    def test_create_user_serializer_password(self):
+        """
+        Password needs to be at least 6 characters long
+        """
+        serializer = self.create_user_serializer
+
+        serializer.initial_data['password'] = 'six'
+        self.assertFalse(serializer.is_valid())
+
+    def test_create_user_serializer_save(self):
+        """
+        Save User object from Create User Serializer
+        """
+        serializer = self.create_user_serializer
+
+        self.assertTrue(serializer.is_valid())
+
+        user = serializer.save()
+        query_user = User.objects.filter(
+            username=serializer.data['username'])
+
+        self.assertTrue(query_user.exists())
+        self.assertEqual(user, query_user.first())
+
+    def test_user_personal_details_serializer(self):
+        """
+        User Personal Details serializer
+        """
+        serializer = self.user_personal_details_serializer
+
+        self.assertTrue(serializer.is_valid())
+
+        data = serializer.data
+
+        self.assertCountEqual(
+            data.keys(), ['username', 'email', 'first_name', 'last_name']
+        )
+        self.assertEqual(data['username'], 'testusername')
+        self.assertEqual(data['email'], 'testemail@example.com')
+        self.assertEqual(data['first_name'], 'Test')
+        self.assertEqual(data['last_name'], 'Tested')
